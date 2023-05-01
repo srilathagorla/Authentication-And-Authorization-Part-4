a7e72c1f3aef22c55616fdff2209bdd1c6245507
srilathagorla <gorlasrilatha07@gmail.com>
2023-05-01 05:22:43 +0000

    solution

---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+import PrimeDealsSection from '../PrimeDealsSection'
+import AllProductsSection from '../AllProductsSection'
+
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-sections">
+      <AllProductsSection />
+    </div>
+  </>
+)
+
+export default Products
